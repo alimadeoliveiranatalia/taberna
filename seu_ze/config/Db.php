9fadc59b3dabc119690ad04f91123840b3262138
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2020-07-06 16:35:48 -0400

    Organizando estrutura de conecxão com DB

--- seu_ze/config/Db.php
+++ seu_ze/config/Db.php
@@ -1,23 +1,13 @@
 <?php
-include("acesso.ini");
 class Db{
     //Atributos
-    private $host;
-    private $user;
-    private $db;
-    private $pass;
-    public $mysqli;
+    private $host='localhost';
+    private $user='root';
+    private $db='taberna';
+    private $pass='';
+    private $mysqli;
     //Métodos
-    public function configurar(){
-        $ini = parse_ini_file("acesso.ini");
-        $this->host = $ini["host"];
-        $this->user = $ini["user"];
-        $this->db = $ini["db"];
-        $this->pass = $ini["pass"];
-
-    }
     public function conectar(){
-        $this->configurar();
         $this->mysqli = new mysqli($this->host,$this->user,$this->db,$this->pass);
         if ($this->mysqli->connect_error) {
             die('Erro de Conecxão ('.$this->mysqli->connect_errno.')'.$this->mysqli->connect_error);
@@ -26,4 +16,13 @@ class Db{
             echo"<center><h1>Conectada :) </h1></center>";
         }
     }
+    public function desconectar(){
+        return mysqli_close($this->mysqli);
+    }
+    public function consultar($sql){
+        $this->mysqli;
+        $r = $this->mysqli->query($sql);
+        return $r;
+    }
+
 }
