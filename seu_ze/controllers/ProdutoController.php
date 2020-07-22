eeff1316b872e83893c67efcafec61e46cdb33aa
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2020-07-22 17:53:03 -0400

    Excluindo $cat

--- seu_ze/controllers/ProdutoController.php
+++ seu_ze/controllers/ProdutoController.php
@@ -4,9 +4,9 @@ require_once '../models/Produto.php';
 class ProdutoController {
     //O controller busca com a Model uma lista de contatos
     // e envia para a view.
-    public function listarTodos($cat = null){
+    public function listarTodos(){
         $chamada = new Produto;
-        $produto=$chamada->listarProduto($cat);
+        $produto = $chamada->listarProduto();
         require_once '../views/prod.php';
     }
     public function listarAlimentos(){
