bf41d16900e7d1b36e4399d93da0613305d4bf66
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2020-07-22 17:56:46 -0400

    Verificando categoria na index

--- seu_ze/views/index.php
+++ seu_ze/views/index.php
@@ -2,11 +2,4 @@
 require_once '../controllers/ProdutoController.php';
 // chamada para script de produtos
 $a = new ProdutoController;
-if (isset($_GET['cat'])){
-    $cat = $_GET['cat'];
-    $a->listarTodos($cat);
-}
-else {
-    $a->listarTodos();
-}
-
+$a->listarTodos();
