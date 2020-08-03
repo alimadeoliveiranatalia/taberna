a272f917eeda381cb3f0ab8240d28a44b3d1cf92
alimadeoliveiranatalia <67019361+alimadeoliveiranatalia@users.noreply.github.com>
2020-08-03 09:36:02 -0400

    Excluindo funções de querys repetidas
    listarAlimentos, listarLimpeza, listarHP e listarBebidas, todas parametrizadas em listarProduto

--- seu_ze/controllers/ProdutoController.php
+++ seu_ze/controllers/ProdutoController.php
@@ -9,25 +9,6 @@ class ProdutoController {
         $produto = $chamada->listarProduto($cat);
         require_once '../views/prod.php';
     }
-    public function listarAlimentos(){
-        $chamada = new Produto;
-        $alimentos = $chamada->listarProdAlimento();
-        require_once '../views/prod_cat_alim.php';
-    }
-    public function listarLimpeza(){
-        $chamada = new Produto;
-        $prodLimpeza = $chamada->listarProdLimpeza();
-        require_once '../views/prod_categoriaLimpeza.php';
-    }
-    public function listarHP(){
-        $chamada = new Produto;
-        $prodHP = $chamada->listarProdHP();
-        require_once '../views/prod_categoriaHP.php';
-    }
-    public function listarBebidas(){
-        $chamada = new Produto;
-        $prodBebidas = $chamada->listarProdBebida();
-        require_once '../views/prod_categoriaBebidas.php';
-    }
+    
 }
 
