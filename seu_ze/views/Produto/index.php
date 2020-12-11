2858ced7e7215098848681fc153d24c58dc41fc0
alimadeoliveiranatalia <67019361+alimadeoliveiranatalia@users.noreply.github.com>
2020-12-11 09:19:46 -0400

    Update index.php
    redirecionando diretorio require_once

--- seu_ze/views/Produto/index.php
+++ seu_ze/views/Produto/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../../controllers/ProdutoController.php';
+require_once '../seu_ze/controllers/ProdutoController.php';
 // chamada para script de produtos
 $a = new ProdutoController;
 if (isset($_GET['cat'])){
