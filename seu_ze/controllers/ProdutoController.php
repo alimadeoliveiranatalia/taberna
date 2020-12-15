283ac633e5b8c0db8303c079da83a09eca1c64a3
alimadeoliveiranatalia <67019361+alimadeoliveiranatalia@users.noreply.github.com>
2020-12-15 11:38:28 -0400

    Update ProdutoController.php
    Redirecionando diretórios depois da criação da index na entrada do projeto

--- seu_ze/controllers/ProdutoController.php
+++ seu_ze/controllers/ProdutoController.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../../models/Produto.php';
+require_once 'models/Produto.php';
 
 class ProdutoController {
     //O controller busca com a Model uma lista de contatos
@@ -7,12 +7,12 @@ class ProdutoController {
     public function listarTodos($cat = null){
         $chamada = new Produto;
         $produto = $chamada->listarProduto($cat);
-        require_once '../../views/Produto/Produto_views.php';
+        require_once 'views/Produto/Produto_views.php';
     }
     public function selecionarProduto($cod){
         $chamada = new Produto;
         $produtoLevar = $chamada->levarProduto($cod);
-        require_once '../../views/Produto/levar.php';
+        require_once 'views/Produto/levar.php';
     }
 }
 
