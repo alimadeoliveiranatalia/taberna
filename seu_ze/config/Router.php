7592a1bad2d823ae4fe13c206e94df4c4fd5feab
alimadeoliveiranatalia <67019361+alimadeoliveiranatalia@users.noreply.github.com>
2020-12-15 11:37:40 -0400

    Update Router.php
    Adcionando outra alternativa para criar um roteamento

--- seu_ze/config/Router.php
+++ seu_ze/config/Router.php
@@ -1,71 +1,30 @@
 <?php
 class Router
 {
-    private $config;
-    private $url;
-    public $mvc;
-    private $params;
-
-    public function __construct()
+    public function __construct($url)
     {
-        $this->config = new Config();
-
-        $this->url=$this->request()['url'];
-        $this->cleanUrl();
-        $this->setMVC();
-        $this->loadMVC();
-
-    }
-
-    /* Remove a primeira barra e também será responsável por limpezas de caracteres na nossa URL em caso de atualizações
-    no nosso framework */
-    private function cleanUrl($url=true, $params=false){
-        if($url){
-            if (preg_match('/.{1,}(/){1}$/', $this->url)){
-                $this->url=substr($this->url,0,-1);
-            }
-        }
-        if($params){
-            $this->params=substr($this->url,1);
-        }
-    }
-    /* Pega a nossa URL devidamente tratada no passo anterior e cria uma matriz(array) para o próximo método */
-    private function urlArray(){
-        if (preg_match('/.{1,}(/){1}$/', $this->url)){
-            $this->url=substr($this->url,0,-1);
-        }
-    }
-    /* Ele vai identificar qual o módulo deve carregar na nossa aplicação a partir do Controller */
-    private function setMVC(){
-        if (isset($this->config->router[$this->url])){
-            $this->mvc = $this->config->router[$this->url];
+        /** 
+         * home/display/ 
+         */
+        $url = rtrim($url,'/');
+         /** 
+         * home/display
+         */
+        $url = explode('/',$url);
+        /*
+        $url[0] = home - controller
+        $url[1] = display - models - method
+        $url[2] = data - views - container
+        */
+        $controller = $url[0].'Çontroller';
+        require_once'../controllers/'.$controller.'.php';
+        $a = new ProdutoController;
+        if (isset($_GET['cat'])){
+         $a->listarTodos($_GET['cat']);
         }
         else{
-            $this->cleanUrl(false,true);
-            $this->params = explode('/', $this->params);
-            $this->params[0] = $this->upperCamelCase($this->params[0]);
-            if ($this->url == '/'){
-                $this->mvc == array(
-                    'model'=>'Index',
-                    'controller' => 'Index',
-                    'action' => 'index',
-                );
-            }
+        $a->listarTodos();
         }
-        if (!isset($this->mvc['action']) || empty($this->mvc['action'])){
-            $this->mvc['action']='Index';
-        }
-        if (!isset($this->mvc['controller']) || empty($this->mvc['controller'])){
-            $this->mvc['controller']='Index';
-        }
-    }
-    /* E por fim este método carrega a Controller específico e já indica  uma função deste controller */
-    private function loadMVC(){
-        $controller = $this->mvc['controller'].'_Controller';
-        $action = $this->mvc['action'];
-
-        $controller = new $controller($this->mvc);
-        $controller->action();
     }
 
 }
