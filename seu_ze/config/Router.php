ffe2d696dd7c7b7b58b2a6e27d312522e2dd2753
alimadeoliveiranatalia <67019361+alimadeoliveiranatalia@users.noreply.github.com>
2020-12-16 01:04:11 -0400

    Update Router.php
    corrigindo class Router

--- seu_ze/config/Router.php
+++ seu_ze/config/Router.php
@@ -3,28 +3,8 @@ class Router
 {
     public function __construct($url)
     {
-        /** 
-         * home/display/ 
-         */
-        $url = rtrim($url,'/');
-         /** 
-         * home/display
-         */
-        $url = explode('/',$url);
-        /*
-        $url[0] = home - controller
-        $url[1] = display - models - method
-        $url[2] = data - views - container
-        */
-        $controller = $url[0].'Çontroller';
-        require_once'../controllers/'.$controller.'.php';
-        $a = new ProdutoController;
-        if (isset($_GET['cat'])){
-         $a->listarTodos($_GET['cat']);
-        }
-        else{
-        $a->listarTodos();
-        }
+        
     }
+    
 
 }
